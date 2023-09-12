69eb345c508403d09baf62cede74112e14054d4c
bhanusrigoda <bhanu-sri.goda@capgemini.com>
2023-09-12 14:27:52 +0530

    add file has multiplications

--- Add.java
+++ Add.java
@@ -2,7 +2,7 @@ public class Add{
   public static void main(String[] args) {
     int x = 5;
     int y = 6;
-    int sum = x + y;
+    int sum = x * y;
     System.out.println(sum); // Print the sum of x + y
   }
 }
