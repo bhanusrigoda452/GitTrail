f313bb5d1f3c3c8a380638d9241c2005395d5bcc
bhanusrigoda <bhanu-sri.goda@capgemini.com>
2023-09-13 12:07:07 +0530

    changes in sub.java file

--- Sub.java
+++ Sub.java
@@ -2,7 +2,6 @@ public class Sub{
   public static void main(String[] args) {
     int x = 5;
     int y = 6;
-    int sum = x - y;
-    System.out.println(sum); // Print the sum of x + y
+    we have to do subtraction
   }
 }
