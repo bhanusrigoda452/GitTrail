48e063142a083997132981d8532284c12b1aaf98
bhanusrigoda <bhanu-sri.goda@capgemini.com>
2023-09-13 11:53:30 +0530

    switched to main subtraction

--- Add.java
+++ Add.java
@@ -3,6 +3,6 @@ public class Add{
     int x = 5;
     int y = 6;
     int sum = x * y;
-    System.out.println(sum); // Print the sum of x + y
+    System.out.println(sum); // Print the subtraction of x - y
   }
 }
